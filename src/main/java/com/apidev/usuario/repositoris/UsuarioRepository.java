4fa00b6bd84645238c11d19cb11b92e4f475f9ba
Camila <alvescamila87@gmail.com>
2025-07-19 19:26:13 -0300

    feat: user repository

---
+++ src/main/java/com/apidev/usuario/repositoris/UsuarioRepository.java
@@ -0,0 +1,13 @@
+package com.apidev.usuario.repositoris;
+
+import com.apidev.usuario.entities.UsuarioEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<UsuarioEntity, Long> {
+
+    Optional<UsuarioEntity> findByEmail(String email);
+}

54a4ad8459ab5af0da4c34ccc3609b7bd2826dac
Camila <alvescamila87@gmail.com>
2025-07-23 18:22:10 -0300

    feat: controller

--- src/main/java/com/apidev/usuario/controllers/UsuarioController.java
+++ src/main/java/com/apidev/usuario/controllers/UsuarioController.java
@@ -1,8 +1,12 @@
 package com.apidev.usuario.controllers;
 
+import com.apidev.usuario.dtos.UsuarioListaDTO;
 import com.apidev.usuario.services.UsuarioService;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/usuario")
@@ -13,4 +17,10 @@ public class UsuarioController {
     public UsuarioController(UsuarioService usuarioService) {
         this.usuarioService = usuarioService;
     }
+
+    @GetMapping
+    public List<UsuarioListaDTO> listarUsuarios() {
+        return usuarioService.listarUsuarios();
+    }
+
 }

c027ff2d8224fa2374c167eea05fc0772a165334
Camila Alves <alvescamila87@gmail.com>
2025-07-25 19:51:44 -0300

    feat: controller

--- src/main/java/com/apidev/usuario/controllers/UsuarioController.java
+++ src/main/java/com/apidev/usuario/controllers/UsuarioController.java
@@ -2,9 +2,7 @@ package com.apidev.usuario.controllers;
 
 import com.apidev.usuario.dtos.UsuarioListaDTO;
 import com.apidev.usuario.services.UsuarioService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -23,4 +21,8 @@ public class UsuarioController {
         return usuarioService.listarUsuarios();
     }
 
+    @DeleteMapping("/{id}")
+    public void deleteUsuario(@PathVariable Long id) {
+        usuarioService.deleteUsuario(id);
+    }
 }

708e090d7ab22c06e0e32b1ead243a7e0f7bad9c
Camila <alvescamila87@gmail.com>
2025-09-20 19:28:44 -0300

    feat: is fetching

--- api-dev-frontend/src/components/Usuario/components/Modal/index.tsx
+++ api-dev-frontend/src/components/Usuario/components/Modal/index.tsx
@@ -58,128 +58,130 @@ export const CreateModal = ({
       <div className="modal-body">
         {idSelected ? <h2>Edição de usuário</h2> : <h2>Dados de usuário</h2>}
 
-        <Box
-          component="form"
-          className="input-container"
-          onSubmit={handleSubmit(onSubmit)}
-          sx={{ display: "flex", flexDirection: "column", gap: 2 }}
-        >
-          <TextField
-            label="Nome"
-            {...register("nome")}
-            defaultValue={""}
-            required
-            fullWidth
-            margin="normal"
-            error={!!errors.nome}
-            helperText={errors.nome?.message}
-            disabled={isView}
-            InputLabelProps={{ shrink: isView }}
-          />
-          <TextField
-            label="E-mail"
-            type="email"
-            {...register("email")}
-            defaultValue={""}
-            required
-            fullWidth
-            margin="normal"
-            error={!!errors.email}
-            helperText={errors.email?.message}
-            disabled={isView}
-            InputLabelProps={{ shrink: isView }}
-          />
-          <TextField
-            label="Senha"
-            type={showPassword ? "text" : "password"}
-            defaultValue={""}
-            {...register("senha")}
-            InputProps={{
-              endAdornment: (
-                <InputAdornment position="end">
-                  <IconButton
-                    onClick={handleClickShowPassword}
-                    onMouseDown={handleMouseDownPassword}
-                    edge="end"
-                  >
-                    {showPassword ? <VisibilityOff /> : <Visibility />}
-                  </IconButton>
-                </InputAdornment>
-              ),
-            }}
-            fullWidth
-            required
-            margin="normal"
-            error={!!errors.senha}
-            helperText={errors.senha?.message}
-            disabled={isView}
-            InputLabelProps={{ shrink: isView }}
-          />
-          <FormControl
-            fullWidth
-            margin="normal"
-            error={!!errors.tipoPermissao}
-            disabled={isView}
-            required
+        {!isFetching && (
+          <Box
+            component="form"
+            className="input-container"
+            onSubmit={handleSubmit(onSubmit)}
+            sx={{ display: "flex", flexDirection: "column", gap: 2 }}
           >
-            <InputLabel>Tipo de Permissão</InputLabel>
-            <Controller
-              name="tipoPermissao"
-              control={control}
-              render={({ field }) => (
-                <Select
-                  labelId="tipo-permissao-label"
-                  label="Tipo de Permissão"
-                  {...field}
-                  disabled={isView}
-                >
-                  <MenuItem value="" disabled>
-                    Selecione uma opção
-                  </MenuItem>
-                  {Object.entries(TipoPermissaoDescription).map(
-                    ([value, label]) => (
-                      <MenuItem key={value} value={value}>
-                        {label}
-                      </MenuItem>
-                    )
-                  )}
-                </Select>
-              )}
+            <TextField
+              label="Nome"
+              {...register("nome")}
+              // defaultValue={""}
+              required
+              fullWidth
+              margin="normal"
+              error={!!errors.nome}
+              helperText={errors.nome?.message}
+              disabled={isView}
+              InputLabelProps={{ shrink: isView }}
             />
-            {errors.tipoPermissao && (
-              <Typography color="error" variant="caption" sx={{ ml: 2 }}>
-                {errors.tipoPermissao.message}
-              </Typography>
-            )}
-          </FormControl>
-          <FormControlLabel
-            control={<Checkbox {...register("ativo")} defaultChecked />}
-            label="Usuário ativo"
-          />
-          {isView && (
-            <div
-              style={{ display: "flex", justifyContent: "flex-end", gap: 5 }}
+            <TextField
+              label="E-mail"
+              type="email"
+              {...register("email")}
+              // defaultValue={""}
+              required
+              fullWidth
+              margin="normal"
+              error={!!errors.email}
+              helperText={errors.email?.message}
+              disabled={isView}
+              InputLabelProps={{ shrink: isView }}
+            />
+            <TextField
+              label="Senha"
+              type={showPassword ? "text" : "password"}
+              // defaultValue={""}
+              {...register("senha")}
+              InputProps={{
+                endAdornment: (
+                  <InputAdornment position="end">
+                    <IconButton
+                      onClick={handleClickShowPassword}
+                      onMouseDown={handleMouseDownPassword}
+                      edge="end"
+                    >
+                      {showPassword ? <VisibilityOff /> : <Visibility />}
+                    </IconButton>
+                  </InputAdornment>
+                ),
+              }}
+              fullWidth
+              required
+              margin="normal"
+              error={!!errors.senha}
+              helperText={errors.senha?.message}
+              disabled={isView}
+              InputLabelProps={{ shrink: isView }}
+            />
+            <FormControl
+              fullWidth
+              margin="normal"
+              error={!!errors.tipoPermissao}
+              disabled={isView}
+              required
             >
-              <Button
-                variant="contained"
-                color="primary"
-                type="submit"
-                disabled={mutateUsuario.isPending || mode === "view"}
+              <InputLabel>Tipo de Permissão</InputLabel>
+              <Controller
+                name="tipoPermissao"
+                control={control}
+                render={({ field }) => (
+                  <Select
+                    labelId="tipo-permissao-label"
+                    label="Tipo de Permissão"
+                    {...field}
+                    disabled={isView}
+                  >
+                    <MenuItem value="" disabled>
+                      Selecione uma opção
+                    </MenuItem>
+                    {Object.entries(TipoPermissaoDescription).map(
+                      ([value, label]) => (
+                        <MenuItem key={value} value={value}>
+                          {label}
+                        </MenuItem>
+                      )
+                    )}
+                  </Select>
+                )}
+              />
+              {errors.tipoPermissao && (
+                <Typography color="error" variant="caption" sx={{ ml: 2 }}>
+                  {errors.tipoPermissao.message}
+                </Typography>
+              )}
+            </FormControl>
+            <FormControlLabel
+              control={<Checkbox {...register("ativo")} defaultChecked />}
+              label="Usuário ativo"
+            />
+            {isView && (
+              <div
+                style={{ display: "flex", justifyContent: "flex-end", gap: 5 }}
               >
-                Salvar
-              </Button>
-              <Button
-                variant="outlined"
-                color="primary"
-                type="reset"
-                onClick={onClose}
-                disabled={mutateUsuario.isPending}
-              >
-                Cancelar
-              </Button>
-            </div>
-          )}
-        </Box>
+                <Button
+                  variant="contained"
+                  color="primary"
+                  type="submit"
+                  disabled={mutateUsuario.isPending || mode === "view"}
+                >
+                  Salvar
+                </Button>
+                <Button
+                  variant="outlined"
+                  color="primary"
+                  type="reset"
+                  onClick={onClose}
+                  disabled={mutateUsuario.isPending}
+                >
+                  Cancelar
+                </Button>
+              </div>
+            )}
+          </Box>
+        )}
         {/* {mutateUsuario.isPending && <Typography>Salvando...</Typography>}
         {mutateUsuario.isError && (
           <Typography color="error">

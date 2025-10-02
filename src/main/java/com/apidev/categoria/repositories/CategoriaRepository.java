2ee2cc62e41426169b490e82aab7bc0668896f14
Camila <alvescamila87@gmail.com>
2025-10-01 21:36:20 -0300

    feat: service

--- src/main/java/com/apidev/categoria/repositories/CategoriaRepository.java
+++ src/main/java/com/apidev/categoria/repositories/CategoriaRepository.java
@@ -1,9 +1,13 @@
 package com.apidev.categoria.repositories;
 
 import com.apidev.categoria.entity.CategoriaEntity;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CategoriaRepository extends JpaRepository<CategoriaEntity, Long> {
+
+    Page<CategoriaEntity> findAll(Pageable pageable);
 }
--- src/main/java/com/apidev/categoria/services/CaetgoriaService.java
+++ src/main/java/com/apidev/categoria/services/CaetgoriaService.java
@@ -1,10 +1,15 @@
 package com.apidev.categoria.services;
 
 import com.apidev.categoria.dtos.CategoriaDTO;
+import com.apidev.categoria.entity.CategoriaEntity;
 import com.apidev.categoria.repositories.CategoriaRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
+import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -13,7 +18,26 @@ public class CaetgoriaService {
 
     private final CategoriaRepository repository;
 
-    public List<CategoriaDTO> findAll(){
-        return null;
+    public Page<CategoriaDTO> findAll(int page, int size) {
+        PageRequest pageRequest = PageRequest.of(page, size, Sort.by("nome").ascending());
+
+        Page<CategoriaEntity> listaCategorias = repository.findAll(pageRequest);
+
+        return listaCategorias.map(CategoriaDTO::of);
+
+    }
+
+    public List<CategoriaDTO> listAll(){
+        List<CategoriaDTO> listaCegoriasDTO = new ArrayList<>();
+
+        List<CategoriaEntity> listaCategoriaEntity = repository.findAll();
+
+        for (CategoriaEntity categoria : listaCategoriaEntity) {
+            CategoriaDTO.of(categoria);
+
+            listaCegoriasDTO.add(CategoriaDTO.of(categoria));
+        }
+
+        return listaCegoriasDTO;
     }
 }

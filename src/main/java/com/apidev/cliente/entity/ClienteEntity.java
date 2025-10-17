7d369b0eb7088f7e37fc6ae154d15b529a129641
Camila Alves <alvescamila87@gmail.com>
2025-10-17 10:25:09 -0300

    feat: entity

--- src/main/java/com/apidev/cliente/entity/ClienteEntity.java
+++ src/main/java/com/apidev/cliente/entity/ClienteEntity.java
@@ -15,4 +15,7 @@ public class ClienteEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    @Column(name = "nome", nullable = false)
+    private String nome;
 }

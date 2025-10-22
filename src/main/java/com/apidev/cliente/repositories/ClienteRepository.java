fc94105a7164f758947f492aa60b83784c5b23ba
Camila Alves <alvescamila87@gmail.com>
2025-10-22 18:59:41 -0300

    feat? repository

---
+++ src/main/java/com/apidev/cliente/repositories/ClienteRepository.java
@@ -0,0 +1,9 @@
+package com.apidev.cliente.repositories;
+
+import com.apidev.cliente.entity.ClienteEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ClienteRepository extends JpaRepository<ClienteEntity, Long> {
+}

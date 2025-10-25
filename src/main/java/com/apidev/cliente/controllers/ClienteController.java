ab3aa87f45b3aff6557876d02bb849d66756dd51
Camila Alves <alvescamila87@gmail.com>
2025-10-25 19:27:22 -0300

    feat: delete controller

---
+++ src/main/java/com/apidev/cliente/controllers/ClienteController.java
@@ -0,0 +1,21 @@
+package com.apidev.cliente.controllers;
+
+import com.apidev.cliente.services.ClienteService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/cliente")
+@RequiredArgsConstructor
+public class ClienteController {
+
+    private final ClienteService clienteService;
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteCliente(@PathVariable Long id){
+        clienteService.deleteCliente(id);
+        return ResponseEntity.noContent().build();
+    }
+
+}

225da4f1fbdb15bb04542785ff67571aa5338b16
Camila <alvescamila87@gmail.com>
2025-10-29 19:54:56 -0300

    feat: controller, service

--- src/main/java/com/apidev/cliente/controllers/ClienteController.java
+++ src/main/java/com/apidev/cliente/controllers/ClienteController.java
@@ -1,6 +1,8 @@
 package com.apidev.cliente.controllers;
 
+import com.apidev.cliente.dtos.ClienteDTO;
 import com.apidev.cliente.services.ClienteService;
+import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -11,6 +13,16 @@ import org.springframework.web.bind.annotation.*;
 public class ClienteController {
 
     private final ClienteService clienteService;
+
+    @PostMapping
+    public ResponseEntity addCliente(@Valid @RequestBody ClienteDTO clienteDTO) {
+        boolean result = clienteService.addCliente(clienteDTO);
+
+        if(!result) {
+            return ResponseEntity.badRequest().build();
+        }
+        return ResponseEntity.ok().build();
+    }
 
     @DeleteMapping("/{id}")
     public ResponseEntity<Void> deleteCliente(@PathVariable Long id){
--- src/main/java/com/apidev/cliente/services/ClienteService.java
+++ src/main/java/com/apidev/cliente/services/ClienteService.java
@@ -1,6 +1,7 @@
 package com.apidev.cliente.services;
 
 import com.apidev.cliente.dtos.ClienteDTO;
+import com.apidev.cliente.entity.ClienteEntity;
 import com.apidev.cliente.repositories.ClienteRepository;
 import com.apidev.usuario.exceptions.ValidationException;
 import lombok.RequiredArgsConstructor;
@@ -12,13 +13,44 @@ public class ClienteService {
 
     private final ClienteRepository clienteRepository;
 
-    public void addCliente(ClientDTO clienteDTO) {
+    public boolean addCliente(ClienteDTO clienteDTO) {
+
+        setValues(clienteDTO);
+
+        clienteRepository.save(ClienteEntity.from(clienteDTO));
+
+        return true;
     }
 
     public void setValues(ClienteDTO clienteDTO) {
         if(clienteDTO == null){
             throw new ValidationException("Campos obrigatórios não preenchidos.");
         }
+
+        if(clienteDTO.getNome().isBlank()) {
+            throw new ValidationException("Nome é um campo obrigatório.");
+        }
+
+        if(clienteDTO.getSobrenome().isBlank()) {
+            throw new ValidationException("Sobrenome é um campo obrigatório.");
+        }
+
+        if(clienteDTO.getTipoPessoa().getDescricao().isBlank()) {
+            throw new ValidationException("Tipo de pessoa é um campo obrigatório.");
+        }
+
+        if(clienteDTO.getDocumento().isBlank()) {
+            throw new ValidationException("Documento é um campo obrigatório.");
+        }
+
+        if(clienteDTO.getDataNascimento() == null) {
+            throw new ValidationException("Data de nascimento é um campo obrigatório.");
+        }
+
+        if(clienteDTO.getEndereco().getCep().isBlank()) {
+            throw new ValidationException("CEP é um campo obrigatório.");
+        }
+
     }
 
     public void deleteCliente(Long id) {

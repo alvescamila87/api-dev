be886611250da6c20b920a48abc90912f4d9251a
Camila <alvescamila87@gmail.com>
2025-11-09 21:44:25 -0300

    feat: repo

---
+++ src/main/java/com/apidev/endereco/repositories/EnderecoRepository.java
@@ -0,0 +1,9 @@
+package com.apidev.endereco.repositories;
+
+import com.apidev.endereco.entity.EnderecoEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EnderecoRepository extends JpaRepository<EnderecoEntity, Long> {
+}

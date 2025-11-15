3f975500d8505c9b73506eb8f36f769bfc82f542
Camila <alvescamila87@gmail.com>
2025-11-15 18:43:55 -0300

    feat: service and controller

--- src/main/java/com/apidev/endereco/controllers/EnderecoController.java
+++ src/main/java/com/apidev/endereco/controllers/EnderecoController.java
@@ -5,6 +5,7 @@ import com.apidev.endereco.services.EnderecoService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -22,5 +23,10 @@ public class EnderecoController {
                     @RequestParam(name = "cep") String cep
      ) {
         return ResponseEntity.ok(enderecoService.findAll(page, size, cep));
+    }
+
+    @DeleteMapping("/id")
+    public ResponseEntity<Void> delete (Long id) {
+        enderecoService.delete(id);
     }
 }
--- src/main/java/com/apidev/endereco/services/EnderecoService.java
+++ src/main/java/com/apidev/endereco/services/EnderecoService.java
@@ -3,6 +3,7 @@ package com.apidev.endereco.services;
 import com.apidev.endereco.dtos.EnderecoDTO;
 import com.apidev.endereco.entity.EnderecoEntity;
 import com.apidev.endereco.repositories.EnderecoRepository;
+import com.apidev.usuario.exceptions.ValidationException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -29,5 +30,16 @@ public class EnderecoService {
         }
 
         return listaEnderecoEntity.map(EnderecoDTO::of);
+    }
+
+    public void delete (Long id) {
+        findById(id);
+        enderecoRepository.deleteById(id);
+    }
+
+    public EnderecoDTO findById(Long id) {
+        EnderecoEntity enderecoEntity = enderecoRepository.findById(id).orElseThrow(() -> new ValidationException("Não foi encontrado endereço com esse ID."));
+
+        return EnderecoDTO.of(enderecoEntity);
     }
 }

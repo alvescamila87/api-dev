5d57228de7d4512de7cf1b143730be5ae211e071
Camila <alvescamila87@gmail.com>
2025-11-25 21:10:00 -0300

    feat: type

---
+++ api-dev-frontend/src/components/Categoria/service/types.ts
@@ -0,0 +1,14 @@
+export type CategoriaResponse = {
+  id: number | null;
+  nome: string;
+  descricao: string | null;
+};
+
+export type CategoriaList = CategoriaResponse[];
+
+export type CategoriaPageableResponse = {
+  content: CategoriaList;
+  totalPages: number;
+  pageNumber: number;
+  pageSize: number;
+};
